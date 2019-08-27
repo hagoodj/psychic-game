01e39c19341a783671b117daf428a9bd2c936ce2
jordan hagood <jordan.hagood@yahoo.com>
2019-08-26 19:07:40 -0700

    random letter from alphabet stored in variable myLetter and logged to console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,11 @@ var losses = 0;
 var guessesRemaining = 10;
 var lettersGuessed = [];
 
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var myLetter = alphabet[Math.floor(Math.random() * 26)];
+
+console.log(myLetter);
+
 document.getElementById("wins").innerHTML = "Wins: " + wins;
 document.getElementById("losses").innerHTML = "Losses: " + losses;
 document.getElementById("guessesRemaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
