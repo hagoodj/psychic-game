ede6b92374ebd57c2d0f8055680fbc9c7e44ba05
jordan hagood <jordan.hagood@yahoo.com>
2019-08-26 18:53:49 -0700

    return js elements to webpage

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
+var lettersGuessed = [];
+
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "Losses: " + losses;
+document.getElementById("guessesRemaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
+document.getElementById("lettersGuessed").innerHTML = "Letters Already Guessed: " + lettersGuessed;
